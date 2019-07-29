8efdbdde49573221c9b2ace08af7b48dc1a1f1dd
ywuanchai <itsmystyle.0821@gmail.com>
2019-07-29 17:41:32 +0800

    set threshold to filter word freq. in make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -52,15 +52,20 @@ def main(args):
     word_dict['<SOS>'] = 2
     word_dict['<EOS>'] = 3
     for word in tqdm(words.most_common()):
-        if word[1] > 1:
+        if word[1] > args.threshold:
             word_dict[word[0]] = counter
             counter += 1
     
-    with open(config['vocab_path'], 'wb') as fout:
+    logging.info('{} words saved'.format(counter))
+    
+    vocab_path = '_{}.pkl'.format(args.threshold).join(config['vocab_path'].split('.pkl'))
+    word_set_path = '_{}.pkl'.format(args.threshold).join(config['word_set_path'].split('.pkl'))
+    
+    with open(vocab_path, 'wb') as fout:
         pickle.dump(word_dict, fout)
-    with open(config['word_set_path'], 'wb') as fout:
+    with open(word_set_path, 'wb') as fout:
         pickle.dump(words, fout)
-    logging.info('Word frequency and vocab saved in {}, {}'.format(config['word_set_path'], config['vocab_path']))
+    logging.info('Word frequency and vocab saved in {}, {}'.format(word_set_path, vocab_path))
 
     # update word dictionary used by preprocessor
     preprocessor.words_dict = word_dict
@@ -68,7 +73,7 @@ def main(args):
     # train
     logging.info('Processing training set from {}'.format(config['train_path']))
     train = preprocessor.get_dataset(train_data, args.n_workers)
-    train_pkl_path = os.path.join(args.dest_dir, 'train.pkl')
+    train_pkl_path = os.path.join(args.dest_dir, 'train_{}.pkl'.format(args.threshold))
     logging.info('Saving training set to {}'.format(train_pkl_path))
     with open(train_pkl_path, 'wb') as f:
         pickle.dump(train, f)
@@ -76,7 +81,7 @@ def main(args):
     # valid
     logging.info('Processing validation set from {}'.format(config['valid_path']))
     valid = preprocessor.get_dataset(valid_data, args.n_workers)
-    valid_pkl_path = os.path.join(args.dest_dir, 'valid.pkl')
+    valid_pkl_path = os.path.join(args.dest_dir, 'valid_{}.pkl'.format(args.threshold))
     logging.info('Saving validation set to {}'.format(valid_pkl_path))
     with open(valid_pkl_path, 'wb') as f:
         pickle.dump(valid, f)
@@ -84,7 +89,7 @@ def main(args):
     # test
     logging.info('Processing testing set from {}'.format(config['test_path']))
     test = preprocessor.get_dataset(test_data, args.n_workers)
-    test_pkl_path = os.path.join(args.dest_dir, 'test.pkl')
+    test_pkl_path = os.path.join(args.dest_dir, 'test_{}.pkl'.format(args.threshold))
     logging.info('Saving testing set to {}'.format(test_pkl_path))
     with open(test_pkl_path, 'wb') as f:
         pickle.dump(test, f)
@@ -92,7 +97,7 @@ def main(args):
     # long test
     logging.info('Processing long corpus testing set from {}'.format(config['long_test_path']))
     long_test = preprocessor.get_dataset(long_test_data, args.n_workers)
-    long_test_pkl_path = os.path.join(args.dest_dir, 'long_test.pkl')
+    long_test_pkl_path = os.path.join(args.dest_dir, 'long_test_{}.pkl'.format(args.threshold))
     logging.info('Saving long corpus testing set to {}'.format(long_test_pkl_path))
     with open(long_test_pkl_path, 'wb') as f:
         pickle.dump(long_test, f)
@@ -113,6 +118,7 @@ def _parse_args():
     parser.add_argument('dest_dir', type=str,
                         help='[input] Path to the directory that .')
     parser.add_argument('--n_workers', type=int, default=8)
+    parser.add_argument('--threshold', type=int, default=0)
     args = parser.parse_args()
     return args
 

f25e7015ac66b22fbfd8835dfa50bf258505b685
ywuanchai <itsmystyle.0821@gmail.com>
2019-10-10 14:35:52 +0800

    save model only when valid better than last valid, lr finder still in progress

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ import traceback
 import json
 from callbacks import ModelCheckpoint, MetricsLogger
 from metrics import *
+from lr_finder import LRFinder
 
 
 def main(args):
@@ -27,33 +28,68 @@ def main(args):
     with open(config['model_parameters']['valid'], 'rb') as f:
         valid = pickle.load(f)
     config['model_parameters']['valid'] = valid
-
-    if config['arch'] == 'Predictor':
-        from predictor import Predictor
-        PredictorClass = Predictor
-
-    predictor = PredictorClass(
-        metrics=[Accuracy()], word_dict=words_dict,
-        **config['model_parameters']
-    )
     
-    metrics_logger = MetricsLogger(
-        os.path.join(args.model_dir, 'log.json')
-    )
-    
-    if args.load is not None:
-        predictor.load(args.load)
-        metrics_logger.load(int(args.load.split('.')[-1]))
+    if args.lr_finder:
+#         logging.info('creating model!')
         
-    model_checkpoint = ModelCheckpoint(
-        os.path.join(args.model_dir, 'model.pkl'),
-        'Accuracy', 1, 'all'
-    )
+#         vocab_size = len(words_dict)
+#         hidden_size = config['model_parameters']['hidden_size']
+#         embedding_size = config['model_parameters']['embedding_size']
+#         use_same_embedding = config['model_parameters']['use_same_embedding']
+        
+#         encoder = EncoderRNN(vocab_size, hidden_size, embedding_size)
+#         decoder = DecoderRNN(hidden_size, vocab_size, embedding_size, use_same_embedding)
+        
+#         criterion = nn.CrossEntropyLoss()
+        
+#         if config['model_parameters']['optimizer'] == 'Adam':
+#             optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), 
+#                                               lr=1e-7, 
+#                                               weight_decay=1e-2)
+#         elif config['model_parameters']['optimizer'] == 'SGD':
+#             optimizer = torch.optim.SGD(list(encoder.parameters()) + list(decoder.parameters()), 
+#                                              lr=1e-7, 
+#                                              momentum=0.9, 
+#                                              weight_decay=1e-2)
+        
+#         lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
+#         lr_finder.range_test(trainloader, end_lr=100, num_iter=100, step_mode="exp")
+        
+#         # use cuda
+#         self.encoder = self.encoder.to(self.device)
+#         self.decoder = self.decoder.to(self.device)
+        pass
+        
+    else:
+        if config['arch'] == 'Predictor':
+            from predictor import Predictor
+            PredictorClass = Predictor
 
-    logging.info('start training!')
-    predictor.fit_dataset(train,
-                          train.collate_fn,
-                          [model_checkpoint, metrics_logger])
+        predictor = PredictorClass(
+            metrics=[Accuracy()], word_dict=words_dict,
+            **config['model_parameters']
+        )
+
+        metrics_logger = MetricsLogger(
+            os.path.join(args.model_dir, 'log.json')
+        )
+
+        if args.load is not None:
+            predictor.load(args.load)
+            try:
+                metrics_logger.load(int(args.load.split('.')[-1]))
+            except:
+                metrics_logger.load(448)
+
+        model_checkpoint = ModelCheckpoint(
+            os.path.join(args.model_dir, 'model.pkl'),
+            'Accuracy', 1, 'max'
+        )
+
+        logging.info('start training!')
+        predictor.fit_dataset(train,
+                              train.collate_fn,
+                              [model_checkpoint, metrics_logger])
 
 
 def _parse_args():
@@ -65,6 +101,7 @@ def _parse_args():
                         help='Device used to train. Can be cpu or cuda:0,'
                         ' cuda:1, etc.')
     parser.add_argument('--load', default=None, type=str)
+    parser.add_argument('--lr_finder', action='store_true')
     args = parser.parse_args()
     return args
 
